4ef56abb3dd581c673155ca0d7eaa9856112f30c
SchalterDev <schalter.dev@gmail.com>
2018-02-03 18:32:06 +0100

    Ready for production

--- src/scripts/ajax_requests.js
+++ src/scripts/ajax_requests.js
@@ -1,7 +1,7 @@
-//const BASE_URL = location.origin;
+const BASE_URL = location.origin;
 
 // only for development
-const BASE_URL = `http://${location.hostname}:3000`;
+//const BASE_URL = `http://${location.hostname}:3000`;
 
 function getRequest(url, callback) {
     const xmlHttp = new XMLHttpRequest();
